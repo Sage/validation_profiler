b37a95dd45270da55058198a83d5b38bf90b4412
lorandsandor <lorand.sandor@sage.com>
2017-08-07 16:29:03 +0100

    Rubocop refactor

--- lib/validation_profiler/rules/validation_rule.rb
+++ lib/validation_profiler/rules/validation_rule.rb
@@ -2,22 +2,22 @@ module ValidationProfiler
   module Rules
     class ValidationRule
       def get_field_value(obj, field)
-        #attempt to get the field value from the object
+        # attempt to get the field value from the object
         field_value = nil
 
-        #check if the object is a hash
+        # check if the object is a hash
         if obj.is_a?(Hash)
           if obj.has_key?(field)
-            #get the field value
+            # get the field value
             field_value = obj[field]
           elsif obj.has_key?(field.to_s)
             field_value = obj[field.to_s]
           end
         else
-          #if the object does not contain the specified field raise an exception
-          raise ValidationProfiler::Exceptions::FieldNotFound.new(field) if !obj.respond_to?(field)
+          # if the object does not contain the specified field raise an exception
+          raise ValidationProfiler::Exceptions::FieldNotFound, field unless obj.respond_to?(field)
 
-          #get the field value
+          # get the field value
           field_value = obj.send(field)
         end
       end
@@ -26,7 +26,7 @@ module ValidationProfiler
         required = attributes[:required]
         required = true if required.nil?
 
-        #check if the field is required
+        # check if the field is required
         return false if required_field?(field_value, required)
         true
       end
@@ -34,9 +34,9 @@ module ValidationProfiler
       private
 
       def required_field?(field_value, required)
-	(field_value.nil? || 
-	  (field_value.respond_to?(:empty?) && field_value.empty?)) && 
-	  !required
+        (field_value.nil? ||
+          (field_value.respond_to?(:empty?) && field_value.empty?)) &&
+          !required
       end
     end
   end
